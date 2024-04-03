a2e15c958d565b8d090d7f8f00ecf85c38425562
sathwik kadiyala <117636462+sathwik-kadiyala@users.noreply.github.com>
2024-04-03 12:36:48 +0530

    delete changes

--- src/Components/Domains.jsx
+++ src/Components/Domains.jsx
@@ -3,27 +3,29 @@ import axios from 'axios';
 
 export default function Domains({ onSelectDomain }) {
     const [domains, setDomains] = useState([]);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
-        axios.get('https://dns-backend-937x.onrender.com/get-domains')
+        axios.get('http://localhost:5000/get-domains')
             .then(response => {
                 // console.log('Domains fetched successfully:', response.data);
                 setDomains(response.data.domains);
             })
             .catch(error => {
-                 alert('To delete the hosted zone delete all records except the first two (which are the SOA and NS records and then delete hosted zone GOTO view for deleting records')
                 console.error('Error fetching domains:', error);
             });
     }, []);
 
     const handleDeleteDomain = (hostedZoneId) => {
-        axios.delete(`https://dns-backend-937x.onrender.com/delete-hosted-zone/${hostedZoneId}`)
+        axios.delete(`http://localhost:5000/delete-hosted-zone/${hostedZoneId}`)
             .then(response => {
                 console.log('Hosted zone deleted successfully:', response.data);
-               
+                setError(false);
                 setDomains(domains.filter(domain => domain.hostedZoneId !== hostedZoneId));
             })
             .catch(error => {
+                setError(true);
+                // alert()
                 console.error('Error deleting hosted zone:', error);
             });
     };
@@ -51,10 +53,13 @@ export default function Domains({ onSelectDomain }) {
                                 <button onClick={() => onSelectDomain(domain)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg  text-sm  w-20 px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">View</button>
                                 <button onClick={() => handleDeleteDomain(domain.hostedZoneId)} className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
                             </td>
+
                         </tr>
                     ))}
                 </tbody>
+              
             </table>
+            {error && <p className='text-red-600 animate-pulse'>To delete the hosted zone delete all records except the first two (which are the SOA and NS records and then delete hosted zone GOTO view for deleting records</p>}
         </div>
     );
 }
